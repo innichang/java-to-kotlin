d5089eb11a9d1ccfb11be2b12bd6fffa2a83cc7f
Inseok Chang <inni.chang95@gmail.com>
2024-04-12 21:25:00 +0900

    Fruit 클래스

--- src/main/java/section4/lec17/Fruit.java
+++ src/main/java/section4/lec17/Fruit.java
@@ -1,12 +1,18 @@
 package section4.lec17;
 
 public class Fruit {
+    private final long id;
     private final String name;
     private final int price;
 
-    public Fruit(String name, int price) {
+    public Fruit(long id, String name, int price) {
+        this.id = id;
         this.name = name;
         this.price = price;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getName() {
