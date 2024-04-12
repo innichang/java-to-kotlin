3ecbff6851c4df3eb0ed212b5c14d52c9d1bd36f
Inseok Chang <inni.chang95@gmail.com>
2024-04-12 21:24:49 +0900

    필터

--- src/main/java/section4/lec17/lec17Java.java
+++ src/main/java/section4/lec17/lec17Java.java
@@ -18,6 +18,15 @@ public class lec17Java {
                 new Fruit("수박", 10_000)
         );
 
+        String targetFruitName = "바나나";
+        targetFruitName = "수박";
+//        filterFruits(fruits, (fruit) -> targetFruitName.equals(fruit.getName()));
+
+        /*Variable used in lambda expression should be final or effectively final
+        Java에서는 람다를 쓸 때 사용할 수 있는 변수에 제약이 있다!
+         */
+
+
     }
 
     private List<Fruit> filterFruits(List<Fruit> fruits, Predicate<Fruit> fruitFilter) {
